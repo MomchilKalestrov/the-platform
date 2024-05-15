3ed3df065fa276e784da348f7c92b83fa7747470
pAx24 <kalestrov.mo@gmail.com>
2024-05-15 20:31:34 +0300

    Added a better nav design

---
+++ src/app/components/listSection.tsx
@@ -0,0 +1,24 @@
+'use client'
+import React from "react";
+
+export default class ListSection extends React.Component<any, any> {
+
+    constructor(props: any) {
+        super(props);
+        this.state = {
+            collapsed: true
+        }
+    }
+
+    UpdateVisibility = () => this.setState({ collapsed: !this.state.collapsed });
+
+    render = () =>
+        <>
+            <p onClick={ this.UpdateVisibility }>{ this.state.collapsed ? '▷' : '▽' } { this.props.title }</p>
+            <div style={ {
+                display: `${this.state.collapsed ? 'none' : 'unset' }`,
+            } }>
+                { this.props.children }
+            </div>
+        </>
+}
